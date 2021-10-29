b87103f620df120e220e14ffdec4807df02095f4
remi.defoor <remi.defoor@student.howest.be>
2021-10-29 16:02:14 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,32 @@
 <?php
 
+use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('get-players');
 });
+
+Route::get('/players', [PlayerController::class, 'getPlayersView'])
+    ->name('get-players');
+
+Route::get('/player/{uuid}/{name}', [PlayerController::class, 'getPlayerView'])
+    ->name('get-player');
+
+Route::get('/player/{uuid}/{name}/edit', [PlayerController::class, 'getPlayerEditView'])
+    ->name('get-player-edit')
+    ->middleware('auth');
+
+Route::post('/player/{uuid}/{name}/edit', [PlayerController::class, 'processPlayerEdit'])
+    ->name('post-player-edit')
+    ->middleware('auth');
+
+Route::get('/player/{uuid}/{name}/add-game-stats', 'getAddGameStatsView')
+    ->name('get-add-game-stats')
+    ->middleware('auth');
+
+Route::post('/player/{uuid}/{name}/add-game-stats', [PlayerController::class, 'processAddGameStats'])
+    ->name('post-add-game-stats')
+    ->middleware('auth');
+
+require __DIR__.'auth.php';
