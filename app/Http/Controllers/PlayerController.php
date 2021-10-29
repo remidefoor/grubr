214545442f93fa62bb0b0e1c28846976e9513aae
remi.defoor <remi.defoor@student.howest.be>
2021-10-29 16:09:50 +0200

    create player controller

---
+++ app/Http/Controllers/PlayerController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PlayerController extends Controller
+{
+    function getPlayersView() {
+
+    }
+
+    function getPlayerView($uuid, $name) {
+
+    }
+
+    function getPlayerEditView($uuid, $name) {
+
+    }
+
+    function processPlayerEdit(Request $request) {
+
+    }
+
+    function getAddGameStatsView($uuid, $name) {
+
+    }
+
+    function processAddGameStats(Request $request) {
+        
+    }
+}
