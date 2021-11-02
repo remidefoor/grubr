625ae934d32f25769217815697e9fecc514c127f
remi.defoor <remi.defoor@student.howest.be>
2021-11-02 18:51:02 +0100

    create models

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function club() {
+        return $this->belongsTo(Club::class);
+    }
+
+    public function statistics() {
+        return $this->hasMany(Statistic::class);
+    }
 }
