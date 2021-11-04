8ade1c13c195dfe625d32e8667d68e1962710ebb
remi.defoor <remi.defoor@student.howest.be>
2021-11-04 16:11:31 +0100

    replace single quotes with double quotes in HTML

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,21 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>scout.me</title>
-    <!-- <link rel='stylesheet' type='text/css' href='{{asset('css/reset.css')}}'/> -->
-    <link rel='stylesheet' type='text/css' href='{{asset('css/style.css')}}'/>
+    <!-- <link rel="stylesheet" type="text/css" href="{{asset("css/reset.css")}}"/> -->
+    <link rel="stylesheet" type="text/css" href="{{asset("css/style.css")}}"/>
     @yield('headInfo')
 </head>
 <body>
     <header>
         <nav>
-            <a href={{route('get-players')}}>players</a>
+            <a href={{route("get-players")}}>players</a>
 
             @guest
-                <a href=''>login</a>  <!-- TODO add route -->
-                <a href=''>register</a>  <!-- TODO add route name -->
+                <a href="">login</a>  <!-- TODO add route -->
+                <a href="">register</a>  <!-- TODO add route name -->
             @else
-                <a href=''>my profile</a>  <!-- TODO add parameter with route -->
-                <a href=''>sign out</a>  <!-- TODO add route name -->
+                <a href="">my profile</a>  <!-- TODO add parameter with route -->
+                <a href="">sign out</a>  <!-- TODO add route name -->
             @endguest
         </nav>
     </header>
--- resources/views/pages/player-edit.blade.php
+++ resources/views/pages/player-edit.blade.php
@@ -1,57 +1,57 @@
 @extends('layouts.master')
 
 @section('main')
-    <form method='POST' action=''>  <!-- TODO add parameter with route -->
+    <form method="POST" action="">  <!-- TODO add parameter with route -->
         @csrf
-        <label for='email'>Email address</label>
-        <input type='email' id='email' name='email' required />
+        <label for="email">Email address</label>
+        <input type="email" id="email" name="email" required />
 
-        <label for='first-name'>First name</label>
-        <input type='text' id='first-name' name='last-name' required />
+        <label for="first-name">First name</label>
+        <input type="text" id="first-name" name="last-name" required />
 
-        <label for='last-name'>Last name</label>
-        <input type='text' id='last-name' name='first-name' required />
+        <label for="last-name">Last name</label>
+        <input type="text" id="last-name" name="first-name" required />
 
         @foreach ($genders as $gender)
-            <label for='{{$gender}}'>{{$gender}}</label>    
-            <input type='radio' id={{$gender}} name='gender' required />
+            <label for="{{$gender}}">{{$gender}}</label>    
+            <input type="radio" id={{$gender}} name="gender" required />
         @endforeach
 
-        <label for='birth-date'>Date of birth</label>
-        <input type='date' id='birth-date' name='birth-date' required />
+        <label for="birth-date">Date of birth</label>
+        <input type="date" id="birth-date" name="birth-date" required />
 
-        <label for='club'>Club</label>
-        <input id='club' name='club' list='clubs' required />
-        <datalist id='clubs' name='clubs'>
+        <label for="club">Club</label>
+        <input id="club" name="club" required list="clubs" />
+        <datalist id="clubs" name="clubs">
             @for ($i = 0; $i < count($clubs); $i++)
                 <option value={{$clubs[$i]->name}} />  <!-- TODO optgroup per country -->
             @endfor
         </datalist>
 
-        <label for='dominant-hand'>Dominant hand</label>
-        <select id='dominant-hand' name='dominant-hand'>
+        <label for="dominant-hand">Dominant hand</label>
+        <select id="dominant-hand" name="dominant-hand">
             @foreach ($dominantHandValues as $dominantHandValue)
                 <option value={{$dominantHandValue}}>{{$dominantHandValue}}</option>
             @endforeach
         </select>
 
-        <label for='position'></label>
-        <select id='position' name='position'>
+        <label for="position"></label>
+        <select id="position" name="position">
             @foreach ($positions as $position)
                 <option value={{$position}}>{{$position}}</option>
             @endforeach
         </select>
 
-        <label for='height'></label>
-        <input type='number' id='height' name='height' required />
+        <label for="height"></label>
+        <input type="number" id="height" name="height" required />
 
-        <label for='weight'></label>
-        <input type='number' id='weight' name='weight' required />
+        <label for="weight"></label>
+        <input type="number" id="weight" name="weight" required />
 
         @guest
-            <input type='submit' id='register' name='register' value='register' />
+            <input type="submit" id="register" name="register" value="register" />
         @else
-        <input type='submit' id='edit-player' name='register' value='update info' />
+        <input type="submit" id="edit-player" name="register" value="update info" />
         @endguest
     </form>
 @endsection
