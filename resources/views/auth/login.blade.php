58eb3387c8d93d4b51046369b7822a33ed6ac896
remi.defoor <remi.defoor@student.howest.be>
2021-11-07 16:41:20 +0100

    style login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,25 @@
 @extends('layouts.master')
 
+@section('headInfo')
+    <link rel="stylesheet" type="text/css" href="{{asset('css/login.css')}}">
+@endsection
+
 @section('main')
-    <form action="">  <!-- TODO add route -->
-        @csrf
+    <div>
+        <form action="{{route('post-login')}}">
+            @csrf
 
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" required autofocus {{old('email')}} />
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required autofocus placeholder="Email" {{old('email')}} />
 
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" required {{old('password')}} />
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required placeholder="Password" {{old('password')}} />
 
-        <input type="submit" id="login" name="login" value="log in" />
-    </form>
+            <input type="submit" id="login" name="login" value="log in" />
+        </form>
+
+        <p>No account?</p>
+
+        <a href="{{route('get-register')}}">Create one</a>
+    </div>
 @endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,4 @@
 const mix = require('laravel-mix');
 
-mix.sass('public/css/style.scss', 'public/css/style.css');
+mix.sass('public/css/style.scss', 'public/css/style.css')
+    .sass('public/css/login.scss', 'public/css/login.css');
