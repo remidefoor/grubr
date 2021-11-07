71285b347752a27637ebbd545795e5ba489f2321
remi.defoor <remi.defoor@student.howest.be>
2021-11-07 14:05:17 +0100

    style master view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,11 +5,11 @@
         @csrf
 
         <label for="email">Email</label>
-        <input type="email" id="email" name="email" required />
+        <input type="email" id="email" name="email" required autofocus {{old('email')}} />
 
         <label for="password">Password</label>
-        <input type="password" id="password" name="password" required />
+        <input type="password" id="password" name="password" required {{old('password')}} />
 
-        <input type="submit" id="login" name="login" value="Login" />
+        <input type="submit" id="login" name="login" value="log in" />
     </form>
 @endsection
