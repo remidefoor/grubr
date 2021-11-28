855d0552a5cdbcdbe9cedbf7c8f536cdec6de30f
remi.defoor <remi.defoor@student.howest.be>
2021-11-28 11:32:21 +0100

    restyle player view

--- resources/views/pages/player.blade.php
+++ resources/views/pages/player.blade.php
@@ -17,43 +17,47 @@
         @else
             <h1>Hi, {{$player->first_name}}</h1>
         @endguest
-        <img src="{{$profilePictureUrl}}" alt="Profile picture." title="Profile picture." />
+        <div id="player-info">
+            <img src="{{$profilePictureUrl}}" alt="Profile picture." title="Profile picture." />
+            <div id="player-details">
+                <p>
+                    <em>Gender:</em>
+                    {{$player->gender}}
+                </p>
+                <p>
+                    <em>Date of birth:</em>
+                    {{$player->birth_date}}
+                </p>
+                <p>
+                    <em>Dominant hand:</em>
+                    {{$player->dominant_hand}}
+                </p>
+                <p>
+                    <em>Position:</em>
+                    {{$player->position}}
+                </p>
+                <p>
+                    <em>Height:</em>
+                    {{$player->height}} m
+                </p>
+                <p>
+                    <em>Weight:</em>
+                    {{$player->weight}} kg
+                </p>
+                <p>
+                    <em>Club:</em>
+                    {{$player->club->name}}
+                </p>
+            </div>
+        </div>
         @if (Auth::id() == $player->uuid)
             <div id="player-controls">
                 <a href="{{route('get-add-statistic', ['uuid' => $player->uuid, 'firstName' => $player->first_name, 'lastName' => $player->last_name])}}">Add statistic</a>
                 <a href="{{route('get-player-edit', ['uuid' => $player->uuid, 'firstName' => $player->first_name, 'lastName' => $player->last_name])}}">Edit profile</a>
             </div>
         @else
-            <a href="mailto:{{$player->email}}">Get in touch</a>
+            <a id="emai" href="mailto:{{$player->email}}">Get in touch</a>
         @endif
-        <p>
-            <em>Gender:</em>
-            {{$player->gender}}
-        </p>
-        <p>
-            <em>Date of birth:</em>
-            {{$player->birth_date}}
-        </p>
-        <p>
-            <em>Dominant hand:</em>
-            {{$player->dominant_hand}}
-        </p>
-        <p>
-            <em>Position:</em>
-            {{$player->position}}
-        </p>
-        <p>
-            <em>Height:</em>
-            {{$player->height}} m
-        </p>
-        <p>
-            <em>Weight:</em>
-            {{$player->weight}} kg
-        </p>
-        <p>
-            <em>Club:</em>
-            {{$player->club->name}}
-        </p>
     </section>
 
     <section id="statistics">
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 const mix = require('laravel-mix');
 
-mix.sass('public/css/style.scss', 'public/css/style.css')
-    .sass('public/css/players/players.scss', 'public/css/players/players.css')
-    .sass('public/css/players/player.scss', 'public/css/players/player.css')
-    .sass('public/css/auth/login.scss', 'public/css/auth/login.css');
+mix.sass('public/css/_style.scss', 'public/css/style.css')
+    .sass('public/css/players/_players.scss', 'public/css/players/players.css')
+    .sass('public/css/players/_player.scss', 'public/css/players/player.css')
+    .sass('public/css/auth/_login.scss', 'public/css/auth/login.css');
