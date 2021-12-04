3200c2606050c75120d992fbfa12a58af118abb0
remi.defoor <remi.defoor@student.howest.be>
2021-12-04 10:21:58 +0100

    finish profile picture input switching

--- public/js/auth/register.js
+++ public/js/auth/register.js
@@ -1,25 +1,16 @@
 'use strict';
 
+let streaming = false;
+
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     // event bindings
-    document.querySelector('#file-input').addEventListener('click', uploadFile);
+    document.querySelector('#file-input').addEventListener('click', selectFile);
     document.querySelector('#use-camera').addEventListener('click', initVideoStream);
+    document.querySelector('#take-picture').addEventListener('click', displayVideoFrame);
+    document.querySelector('#take-new-picture').addEventListener('click', displayVideoStream);
 }
-
-/* function displayProfilePicture() {
-    const WIDTH = 400;
-    const HEIGHT = WIDTH;
-
-    const $canvas = document.querySelector('#output canvas');
-    const context = $canvas.getContext('2d');
-
-    const $currentProfilePicture = document.querySelector('#current-profile-picture');
-    $canvas.width = WIDTH;
-    $canvas.height = HEIGHT;
-    $currentProfilePicture.onload = () => context.drawImage($currentProfilePicture, 0, 0, WIDTH, HEIGHT);
-} */
 
 function updateProfilePictureSetup(visibleElem1, visibleElem2) {
     const $profilePictureElems = document.querySelector('#profile-picture').children;
@@ -40,6 +31,22 @@ function updateProfilePictureSetup(visibleElem1, visibleElem2) {
     visibleElem2.classList.remove('hidden');
 }
 
+function stopVideoStream() {
+    const stream = document.querySelector('#video-input').srcObject;
+    stream.getTracks().forEach(track => track.stop());
+    streaming = false;
+}
+
+function selectFile(e) {
+    if (streaming) {
+        stopVideoStream();
+        updateProfilePictureSetup(
+            document.querySelector('#output'),
+            document.querySelector('#use-camera')
+        );
+    }
+}
+
 function uploadFile(e) {
     
 }
@@ -47,7 +54,7 @@ function uploadFile(e) {
 function initVideoStream(e) {
     e.preventDefault();
 
-    if ('mediaDevices' in navigator) {
+    if (!streaming && 'mediaDevices' in navigator) {
         const constraints = { video: true };
 
         navigator.mediaDevices.getUserMedia(constraints)
@@ -55,13 +62,9 @@ function initVideoStream(e) {
                 const $videoInput = document.querySelector('#video-input');
                 $videoInput.srcObject = stream;
                 $videoInput.play();
+                streaming = true;
 
-                const $takePicture = document.querySelector('#take-picture');
-
-                $takePicture.addEventListener('click', displayVideoFrame);
-                document.querySelector('#take-new-picture').addEventListener('click', displayVideoStream);
-
-                updateProfilePictureSetup($videoInput, $takePicture);
+                updateProfilePictureSetup($videoInput, document.querySelector('#take-picture'));
             });
     }
 }
@@ -69,24 +72,28 @@ function initVideoStream(e) {
 function displayVideoFrame(e) {
     e.preventDefault();
 
-    const $videoInput = document.querySelector('#video-input');
-    const $canvas = document.querySelector('canvas');
-    const context = $canvas.getContext('2d');
-    const $output = document.querySelector('#output');
+    if (streaming) {
+        const $videoInput = document.querySelector('#video-input');
+        const $canvas = document.querySelector('canvas');
+        const context = $canvas.getContext('2d');
+        const $output = document.querySelector('#output');
 
-    context.clearRect(0, 0, $canvas.width, $canvas.height);
-    context.drawImage($videoInput, 0, 0);
+        context.clearRect(0, 0, $canvas.width, $canvas.height);
+        context.drawImage($videoInput, 0, 0);
 
-    $output.src = $canvas.toDataURL();
+        $output.src = $canvas.toDataURL();
 
-    updateProfilePictureSetup($output, document.querySelector('#take-new-picture'));
+        updateProfilePictureSetup($output, document.querySelector('#take-new-picture'));
+    }
 }
 
 function displayVideoStream(e) {
     e.preventDefault();
 
-    updateProfilePictureSetup(
-        document.querySelector('#video-input'),
-        document.querySelector('#take-picture')
-    );
+    if (streaming) {
+        updateProfilePictureSetup(
+            document.querySelector('#video-input'),
+            document.querySelector('#take-picture')
+        );
+    }
 }
